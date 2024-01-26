0f9eab686b866c26ab75db6f76bfd2307f92cf9e
Tung <89498812+tungdo0602@users.noreply.github.com>
2024-01-26 20:15:08 +0700

    bug fixed & improvements

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ var cfg = { //Example config
     "channel_id": "",
     "presets": {
         "owo": {
-            "default_cooldown": 15500,
+            "default_cooldown": [15250, 16500],
             "bot": {
                 "id": "408785106942164992",
                 "dmid": "" //Bot direct message channel id
@@ -37,6 +37,10 @@ function savecfg(){
     });
 }
 
+function rnd(min=0, max=1, d=2) {
+    return Number((Math.random() * (max - min) + min).toFixed(d))
+}
+
 try {
     let td = fs.readFileSync("./config.json", {encoding: "utf8", "flag": "r"});
     if(td){
@@ -59,10 +63,14 @@ function rndc(a){
 const client = new Client();
 
 const pfx = ".";
-var isStart = false;
-var l;
-var o;
-var mc;
+var isStart = false,
+    l, o, mc,
+    captchaChecked = false;
+
+function stopL(){
+    isStart = false;
+    clearInterval(l);
+}
 
 client.on('ready', async () => {
     console.log(`${client.user.username} is ready!`);
@@ -73,53 +81,67 @@ client.on("messageCreate", function(msg) {
     if(msg.author.id == client.user.id){
         c = msg.content.toLowerCase();
         var args = c.split(" ");
-        if(c.startsWith(pfx + "obt")){
-            o = args[1];
-            if (o&&cfg.presets[o]&&isStart==false){
-                isStart = true;
-                msg.channel.send(`**[${o}]** _Started!_ `);
-                console.log(`[${o}] Started!`);
-                l = setInterval(function handle(){
-                    for(let i of cfg.presets[o].cmds){
-                        if(typeof i[1] != "object"){
-                            i[1] = Array(i[1]);
+        if(c[0] == pfx){
+            let scmd = args[0].slice(1);
+            switch(scmd){
+                case "obt":
+                    o = args[1];
+                    if (o&&cfg.presets[o]&&isStart==false){
+                    isStart = true;
+                    msg.channel.send(`**[${o}]** _Started at <#${cfg.channel_id}>!_`);
+                    console.log(`[${o}] Started!`);
+                    (function handle(){
+                        captchaChecked = false;
+                        for(let i of cfg.presets[o].cmds){
+                            if(typeof i[1] != "object"){
+                                i[1] = Array(i[1]);
+                            }
+                            let cmd = rndc(i[1])
+                            mc.sendTyping();
+                            switch(i[0]){
+                                case "msg":
+                                    mc.send(cmd);
+                                    console.log("[+] Sent msg '" + cmd + "'!");
+                                    break;
+                                case "slash":
+                                    mc.sendSlash(cfg.presets[o].bot.id, cmd);
+                                    console.log("[+] Sent slash '" + cmd + "'!");
+                                    break;
+                                }
                         }
-                        let cmd = rndc(i[1])
-                        switch(i[0]){
-                            case "msg":
-                                mc.send(cmd);
-                                console.log("[+] Sent msg '" + cmd + "'!");
-                                break;
-                            case "slash":
-                                mc.sendSlash(cfg.presets[o].bot.id, cmd);
-                                console.log("[+] Sent slash '" + cmd + "'!");
-                                break;
+                        if((typeof cfg.presets[o].default_cooldown) == "number"){
+                            setTimeout(handle, cfg.presets[o].default_cooldown);
+                        } else {
+                            setTimeout(handle, rnd(...cfg.presets[o].default_cooldown, 0));
                         }
+                    })();
+                    } else {
+                        stopL();
+                        msg.channel.send(`**[+]** _Stopped!_`);
+                        console.log(`[+] Stopped!`);
                     }
-                }, cfg.presets[o].default_cooldown)
-            } else {
-                isStart = false;
-                msg.channel.send(`**[+]** _Stopped!_`);
-                console.log(`[+] Stopped!`);
-                clearInterval(l);
-            }
-        } else if (c.startsWith(pfx + "setchannel")){
-            if(args[1]){
-                msg.channel.send(".obt");
-                cfg.channel_id = args[1].trim();
-                savecfg();
-                mc.send("_Changed Channel ID to_ **" + cfg.channel_id + "**");
-                console.log("Changed Channel ID to " + cfg.channel_id);
-                mc = client.channels.cache.get(cfg.channel_id);
-            } else {
-                mc.send("_Please provide Channel ID!_")
+                    break;
+                case "setchannel":
+                    if(args[1]){
+                        cfg.channel_id = args[1].trim();
+                        savecfg();
+                        mc.send("_Changed Channel ID to_ **" + cfg.channel_id + "**");
+                        console.log("Changed Channel ID to " + cfg.channel_id);
+                        mc = client.channels.cache.get(cfg.channel_id);
+                    } else {
+                        mc.send("_Please provide Channel ID!_")
+                    }
+                    break;
             }
         }
     }
     if(isStart){
         if(msg.author.id == cfg.presets[o].bot.id && (msg.channelId == cfg.channel_id || msg.channelId == cfg.presets[o].bot.dmid)){
-            if(msg.content.toLowerCase().includes("captcha")){
-                prompt("[!] Please verify captcha to continue... ");
+            if(msg.content.toLowerCase().includes("captcha") && !captchaChecked){
+                captchaChecked = true;
+                mc.send(`<@${client.user.id}>`).then(e=>{
+                    e.markUnread().then(()=>prompt("[!] Please verify captcha to continue... "));
+                });
             }
         }
     }
